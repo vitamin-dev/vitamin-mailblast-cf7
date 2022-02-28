cdaa284956d016d2fe4c0c8965b471781bffd8f7
Paul Welbourn <paul@vitaminisgood.com>
2022-02-28 17:20:39 -0500

    Update campaignmonitor

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,33 +1,33 @@
 <?php return array (
   'root' => 
   array (
-    'pretty_version' => '1.0.0+no-version-set',
-    'version' => '1.0.0.0',
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
     'aliases' => 
     array (
     ),
-    'reference' => NULL,
+    'reference' => '54b09959c200eebc90b008567a50c632c552d105',
     'name' => '__root__',
   ),
   'versions' => 
   array (
     '__root__' => 
     array (
-      'pretty_version' => '1.0.0+no-version-set',
-      'version' => '1.0.0.0',
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
       'aliases' => 
       array (
       ),
-      'reference' => NULL,
+      'reference' => '54b09959c200eebc90b008567a50c632c552d105',
     ),
     'campaignmonitor/createsend-php' => 
     array (
-      'pretty_version' => 'v6.1.1',
-      'version' => '6.1.1.0',
+      'pretty_version' => 'v7.0.0',
+      'version' => '7.0.0.0',
       'aliases' => 
       array (
       ),
-      'reference' => '12a4c1f33b940775bde80ae1494036df06ed36d4',
+      'reference' => '3ac6d4d2721f6529a2cae2515f7e88a17baa7d19',
     ),
   ),
 );
